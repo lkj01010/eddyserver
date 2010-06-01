78920de5854d293c27ae1a8e61eb7841892056fb
cometliao <cometliao@1cded24a-c7e3-8868-7c77-85fe8f186616>
2010-06-01 12:50:19 +0000

    CMake自动查找依赖的脚本，以及自动区分链接debug和release版本

---
+++ CMake/Dependencies.cmake
@@ -0,0 +1,69 @@
+set(EDDY_DEPENDENCIES_DIR "" CACHE PATH "Path to prebuilt EDDY dependencies")
+include(FindPkgMacros)
+include(MacroLogFeature)
+getenv_path(EDDY_DEPENDENCIES_DIR)
+
+  set(EDDY_DEP_SEARCH_PATH 
+    ${EDDY_DEPENDENCIES_DIR}
+    ${ENV_EDDY_DEPENDENCIES_DIR}
+    "${EDDY_BINARY_DIR}/dependencies"
+    "${EDDY_SOURCE_DIR}/dependencies"
+    "${EDDY_BINARY_DIR}/../dependencies"
+    "${EDDY_SOURCE_DIR}/../dependencies"
+  )
+
+
+message(STATUS "Search path: ${EDDY_DEP_SEARCH_PATH}")
+
+# Set hardcoded path guesses for various platforms
+if (UNIX)
+  set(EDDY_DEP_SEARCH_PATH ${EDDY_DEP_SEARCH_PATH} /usr/local)
+endif ()
+
+# give guesses as hints to the find_package calls
+set(CMAKE_PREFIX_PATH ${EDDY_DEP_SEARCH_PATH} ${CMAKE_PREFIX_PATH})
+set(CMAKE_FRAMEWORK_PATH ${EDDY_DEP_SEARCH_PATH} ${CMAKE_FRAMEWORK_PATH})
+
+#######################################################################
+# Core dependencies
+#######################################################################
+set(Boost_USE_STATIC_LIBS TRUE)
+set(Boost_ADDITIONAL_VERSIONS "1.42" "1.42.0" "1.43" "1.43.0")
+
+if (UNIX)
+# Components that need linking (NB does not include header-only components like bind)
+set(EDDY_BOOST_COMPONENTS thread date_time system)
+find_package(Boost COMPONENTS ${EDDY_BOOST_COMPONENTS} QUIET)
+if (NOT Boost_FOUND)
+	# Try again with the other type of libs
+	set(Boost_USE_STATIC_LIBS NOT ${Boost_USE_STATIC_LIBS})
+	find_package(Boost COMPONENTS ${EDDY_BOOST_COMPONENTS} QUIET)
+endif()
+macro_log_feature(Boost_SYSTEM_FOUND "boost" "Boost (general)" "http://boost.org" FALSE "" "")
+macro_log_feature(Boost_THREAD_FOUND "boost-thread" "Used for threading support" "http://boost.org" FALSE "" "")
+macro_log_feature(Boost_DATE_TIME_FOUND "boost-date_time" "Used for threading support" "http://boost.org" FALSE "" "")
+endif (UNIX)
+
+find_package(Boost)
+# Optional Boost libs (Boost_${COMPONENT}_FOUND
+macro_log_feature(Boost_FOUND "boost" "Boost (general)" "http://boost.org" FALSE "" "")
+
+find_package(Protobuf)
+  
+# Display results, terminate if anything required is missing
+MACRO_DISPLAY_FEATURE_LOG()
+
+# Add library and include paths from the dependencies
+include_directories(
+  ${Protobuf_INCLUDE_DIRS}
+)
+
+link_directories(
+  ${Protobuf_LIBRARY_DIRS}
+)
+
+if (Boost_FOUND)
+  include_directories(${Boost_INCLUDE_DIRS})
+  link_directories(${Boost_LIBRARY_DIRS})
+endif ()
+
---
+++ CMake/Packages/FindProtobuf.cmake
@@ -0,0 +1,96 @@
+#-------------------------------------------------------------------
+# This file is part of the CMake build system for EDDY
+#     (Object-oriented Graphics Rendering Engine)
+# For the latest info, see http://www.EDDY3d.org/
+#
+# The contents of this file are placed in the public domain. Feel
+# free to make use of it in any way you like.
+#-------------------------------------------------------------------
+
+include(FindPkgMacros)
+findpkg_begin(Protobuf)
+
+# Get path, convert backslashes as ${ENV_${var}}
+getenv_path(Protobuf_HOME)
+getenv_path(EDDY_SDK)
+getenv_path(EDDY_HOME)
+getenv_path(EDDY_SOURCE)
+
+# construct search paths
+set(Protobuf_PREFIX_PATH 
+  ${EDDY_SOURCE}/dependencies ${ENV_EDDY_SOURCE}/dependencies
+  ${EDDY_SDK} ${ENV_EDDY_SDK}
+  ${EDDY_HOME} ${ENV_EDDY_HOME})
+create_search_paths(Protobuf)
+# redo search if prefix path changed
+clear_if_changed(Protobuf_PREFIX_PATH
+  Protobuf_LIBRARY_FWK
+  Protobuf_LIBRARY_REL
+  Protobuf_LIBRARY_DBG
+  Protobuf_INCLUDE_DIR
+)
+
+if (WIN32)
+set(Protobuf_LIBRARY_NAMES libprotobuf)
+else (WIN32)
+set(Protobuf_LIBRARY_NAMES protobuf)
+endif (WIN32)
+
+get_debug_names(Protobuf_LIBRARY_NAMES)
+
+use_pkgconfig(Protobuf_PKGC Protobuf)
+
+# For Protobuf, prefer static library over framework (important when referencing Protobuf source build)
+set(CMAKE_FIND_FRAMEWORK "LAST")
+
+findpkg_framework(Protobuf)
+
+find_path(Protobuf_INCLUDE_DIR NAMES "google/protobuf/dynamic_message.h" "google/protobuf/wire_format_lite.h" HINTS ${Protobuf_INC_SEARCH_PATH} ${Protobuf_PKGC_INCLUDE_DIRS} PATH_SUFFIXES "")
+find_library(Protobuf_LIBRARY_REL NAMES ${Protobuf_LIBRARY_NAMES} HINTS ${Protobuf_LIB_SEARCH_PATH} ${Protobuf_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
+find_library(Protobuf_LIBRARY_DBG NAMES ${Protobuf_LIBRARY_NAMES_DBG} HINTS ${Protobuf_LIB_SEARCH_PATH} ${Protobuf_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
+make_library_set(Protobuf_LIBRARY)
+
+findpkg_finish(Protobuf)
+
+# protoc
+findpkg_begin(Protoc)
+
+# Get path, convert backslashes as ${ENV_${var}}
+getenv_path(Protoc_HOME)
+getenv_path(EDDY_SDK)
+getenv_path(EDDY_HOME)
+getenv_path(EDDY_SOURCE)
+
+# construct search paths
+set(Protoc_PREFIX_PATH 
+  ${EDDY_SOURCE}/dependencies ${ENV_EDDY_SOURCE}/dependencies
+  ${EDDY_SDK} ${ENV_EDDY_SDK}
+  ${EDDY_HOME} ${ENV_EDDY_HOME})
+create_search_paths(Protoc)
+# redo search if prefix path changed
+clear_if_changed(Protoc_PREFIX_PATH
+  Protoc_LIBRARY_FWK
+  Protoc_LIBRARY_REL
+  Protoc_LIBRARY_DBG
+  Protoc_INCLUDE_DIR
+)
+
+if (WIN32)
+set(Protoc_LIBRARY_NAMES libprotoc)
+else (WIN32)
+set(Protoc_LIBRARY_NAMES protoc)
+endif (WIN32)
+get_debug_names(Protoc_LIBRARY_NAMES)
+
+use_pkgconfig(Protoc_PKGC Protoc)
+
+findpkg_framework(Protoc)
+
+find_path(Protoc_INCLUDE_DIR NAMES "google/protobuf/dynamic_message.h" "google/protobuf/wire_format_lite.h" HINTS ${Protoc_INC_SEARCH_PATH} ${Protoc_PKGC_INCLUDE_DIRS} PATH_SUFFIXES "")
+find_library(Protoc_LIBRARY_REL NAMES ${Protoc_LIBRARY_NAMES} HINTS ${Protoc_LIB_SEARCH_PATH} ${Protoc_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
+find_library(Protoc_LIBRARY_DBG NAMES ${Protoc_LIBRARY_NAMES_DBG} HINTS ${Protoc_LIB_SEARCH_PATH} ${Protoc_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
+make_library_set(Protoc_LIBRARY)
+
+findpkg_finish(Protoc)
+# Reset framework finding
+set(CMAKE_FIND_FRAMEWORK "FIRST")
