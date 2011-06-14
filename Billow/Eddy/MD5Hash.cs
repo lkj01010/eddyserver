239485e469898a60f55439c2990e010dc979b9c4
cometliao <cometliao@gmail.com>
2011-06-14 03:06:24 +0000

    增加对二进制和文件取md5的支持

--- Billow/Eddy/MD5Hash.cs
+++ Billow/Eddy/MD5Hash.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -10,8 +11,25 @@ namespace Eddy
     {
         public static string Get(string input)
         {
+            return Get(Encoding.Default.GetBytes(input));
+        }
+
+        public static string Get(byte[] input)
+        {
             MD5 md5Hasher = MD5.Create();
-            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            byte[] data = md5Hasher.ComputeHash(input);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var c in data)
+                stringBuilder.Append(c.ToString("x2"));
+
+            return stringBuilder.ToString();
+        }
+
+        public static string Get(Stream stream)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] data = md5.ComputeHash(stream);
             StringBuilder stringBuilder = new StringBuilder();
 
             foreach (var c in data)
@@ -21,6 +39,20 @@ namespace Eddy
         }
 
         public static bool Verify(string input, string hash)
+        {
+            string hashOfInput = Get(input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            return (0 == comparer.Compare(hashOfInput, hash));
+        }
+
+        public static bool Verify(byte[] input, string hash)
+        {
+            string hashOfInput = Get(input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            return (0 == comparer.Compare(hashOfInput, hash));
+        }
+
+        public static bool Verify(Stream input, string hash)
         {
             string hashOfInput = Get(input);
             StringComparer comparer = StringComparer.OrdinalIgnoreCase;
