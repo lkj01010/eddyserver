cf0cf39c0ef7da9c38892eb249bdd8e188265508
cometliao <cometliao@gmail.com>
2013-03-28 15:06:53 +0000

    工程结构调整，增加EchoServer和EchoClient测试

---
+++ Billow/EchoClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eddy;
+using Eddy.JsonFxMessage;
+using System.Net;
+using Eddy.Net;
+
+class Message
+{
+    public int ID;
+    public string Name;
+}
+
+namespace EchoClient
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDispatcher dispatcher;
+            MessageTcpClient client;
+            dispatcher = SimpleDispatcher.CurrentDispatcher;
+            var message = new Message { ID = 7, Name = "abc" };
+            var serializer = new MessageSerializer();
+            serializer.Register<Message>();
+
+            // 初始化client
+            client = new MessageTcpClient(serializer, (session, o) =>
+            {
+                dispatcher.Invoke(() =>
+                {
+                    Console.WriteLine((o as Message).ID);
+                    Console.WriteLine((o as Message).Name);
+                });
+            });
+            client.Connected += () =>
+            {
+                Console.WriteLine("connected");
+                client.Send(message);
+            };
+            client.Disconnected += (e) =>
+            {
+                Console.WriteLine("disconnected:" + e.Message);
+            };
+
+            var ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            client.Connect(new IPEndPoint(ip, 9528));
+            dispatcher.Run();
+        }
+    }
+
+}
---
+++ Billow/EchoServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eddy;
+using Eddy.JsonFxMessage;
+using System.Net;
+using Eddy.Net;
+class Message
+{
+    public int ID;
+    public string Name;
+}
+
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dispatcher = SimpleDispatcher.CurrentDispatcher;
+            var serializer = new MessageSerializer();
+            serializer.Register<Message>();
+
+            // 初始化service
+            var service = new MessageTcpService(serializer, (session, o) =>
+            {
+                dispatcher.Invoke(() =>
+                {
+                    session.Send(o);
+                });
+            });
+            service.MessageDeserializeFailed += (e) => { Console.WriteLine(e.Message); };
+            var ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            service.Listen(new IPEndPoint(ip, 9528));
+            dispatcher.Run();
+        }
+    }
+}
